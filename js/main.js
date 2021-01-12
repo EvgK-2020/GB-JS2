021a812dd369a41358d9d91dbae0edbc1cd983b1
EvgK-2020 <69770881+EvgK-2020@users.noreply.github.com>
2021-01-13 00:29:38 +0300

    Leson - 1

--- js/main.js
+++ js/main.js
@@ -6,13 +6,17 @@ const products = [
 ];
 
 
-const renderProduct = (title = "", price = "") => `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+const renderProduct = (id, title = "Название товара", price = "0.00", image = "placeholder-250x250.png", description = "Описание товара") => `<div class="product-item">
+                <img src="img/${image}" alt="${title}" class="product-img">
+                <h3 class="product-item-title">${title}</h3>
+                <p class="product-item-description">${description}</p>
+                <p class="product-item-price">${price} руб.</p>
+                <button class="btn-cart" data-id="${id}">Добавить</button>
             </div>`;
 
 const render = productsList => {
-    const productsElements = productsList.map(item => renderProduct(item.title, item.price));
+    const productsElements = productsList.map(item => renderProduct(item.id, item.title, item.price, item.image, item.description));
+
     for (let i = 0; i < productsElements.length; i++) {
         document.querySelector('.products').innerHTML += productsElements[i];
     }
